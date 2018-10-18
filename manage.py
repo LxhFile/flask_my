215e82f4d13f38e71332dd1b4e8cfbc92df673f6
LxhFile <624092049@qq.com>
2018-10-18 21:18:40 +0800

    [新建]空的py文件

--- manage.py
+++ manage.py
@@ -2,4 +2,25 @@
 # @TIME     :2018/10/18  20:59
 # @AUTHOR   :LXH
 # @FILE     :manage
-# @EXPLAIN  :
+# @EXPLAIN  : 程序运行入口
+
+from flask import Flask, render_template
+from flask_script import Manager
+
+
+
+app = Flask(__name__)
+
+
+
+# manager = Manager(app)
+
+@app.route('/')
+def hello():
+    return 'ok'
+
+
+
+if __name__ == '__main__':
+    # manager.run()
+    app.run()
